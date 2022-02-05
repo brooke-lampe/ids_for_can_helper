aae0f44e448504c3bb154f6aea80f3b3df12c366
Brooke Lampe <belampe@gmail.com>
2022-02-04 18:11:07 -0600

    Matched development/test harness with Android app

--- src/com/virtual/Main.java
+++ src/com/virtual/Main.java
@@ -6,11 +6,15 @@ import tel.schich.javacan.util.CanBroker;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 
 public class Main {
 
     public static final NetworkDevice CAN_INTERFACE = lookupDev();
+    public static HashMap<String, Set<String>> ATMAMap = new HashMap<>();
 
     private static NetworkDevice lookupDev() {
         try {
@@ -37,6 +41,18 @@ public class Main {
                 frame_data = frame_data.split("=")[1];
                 frame_data = frame_data.replaceAll("[\\[\\],)]", "");
                 System.out.println("frame_id: " + frame_id + ", frame_data: " + frame_data);
+
+                Set<String> data;
+                if (ATMAMap.containsKey(frame_id)) {
+                    data = ATMAMap.get(frame_id);
+                } else {
+                    data = new HashSet<>();
+                }
+                data.add(frame_data);
+                ATMAMap.put(frame_id, data);
+
+                System.out.println("ATMAMap");
+                System.out.println(ATMAMap);
             });
             canBroker.addFilter(CanFilter.ANY);
         } catch (IOException e) {
